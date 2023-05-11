ae79b044d7546c6d92dfaea2d7a036d44c9bec0b
Min Ji Seo <seominji92@gmail.com>
2023-05-11 00:44:38 -0700

    Axios Test

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const setup = () => {
-  console.log("jQuery Test!");
+const setup = async () => {
+  // test out poke api using axios here
+  const res = await axios.get('https://pokeapi.co/api/v2/pokemon/1')
+  console.log(res.data);
 }
 $(document).ready(setup)
