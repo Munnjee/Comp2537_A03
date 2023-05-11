fb97d91d1e27b504a13c03b88dd965ba3b832da3
Min Ji Seo <seominji92@gmail.com>
2023-05-11 01:20:40 -0700

    Add Filter function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const updatePaginationDiv = (currentPage, numPages) => {
 
   // Ensures page buttons are always between 1 and numPages and current page is in the center
   const startPage = Math.max(1, currentPage - Math.floor(btnPerPage / 2));
-  const endPage = Math.min(numPages, currentPage + Math.floor(btnPerPage /2));
+  const endPage = Math.min(numPages, currentPage + Math.floor(btnPerPage / 2));
 
   // Previous Button
   if (currentPage > 1) {
@@ -58,14 +58,68 @@ const paginate = async (currentPage, PAGE_SIZE, pokemons) => {
   );
 };
 
-
 const setup = async () => {
+  //Create Filter
+  $("#filter").empty();
+  let resType = await axios.get("https://pokeapi.co/api/v2/type");
+  let types = resType.data.results;
+  const filterTypes = types.map((pokemonType) => pokemonType.name);
+  filterTypes.forEach((type) => {
+    $("#filter").append(
+      `<input type="checkbox" id="${type}" class="typeCheckbox" name="type" value="${type}"/>  
+    <label for="${type}">${type}</label>  `);
+  });
 
+  //Function to retrieve pokemon types
+  const getTypes = async (pokemonName) => {
+    const res = await axios.get(`
+    https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    return res.data.types.map((type) => type.type.name);
+  };
+
+  // Event listener to Filter
+  $('body').on('change', '.typeCheckbox', async function (e) {
+    // Get all checked type checkboxes
+    const $selectedTypes = $("input[name='type']:checked");
+    // Extract their values into an array
+    const selectedTypes = $selectedTypes.map(function () {
+      return this.value;
+    }).get();
+
+    // If there are any selected types
+    if (selectedTypes.length > 0) {
+      // Filter the pokemon array using async/await and Array.every()
+      let filteredTypes = await Promise.all(
+        pokemons.map(async (pokemon) => {
+          const pokemonTypes = await getTypes(pokemon.name);
+          return selectedTypes.every((type) =>
+            pokemonTypes.includes(type))
+            ? pokemon : null;
+        })
+      );
+      // Remove any null values from the filteredTypes array
+      pokemons = filteredTypes.filter((p) => p !== null);
+    } else {
+      // If no types are selected, reset the pokemon array to its original state
+      pokemons = response.data.results;
+    }
+    // Paginate the filtered pokemon array using the "paginate" function
+    paginate(currentPage, PAGE_SIZE, pokemons);
+    // Calculate the total number of pages based on the filtered pokemon array length and page size
+    let numPages = Math.ceil(pokemons.length / PAGE_SIZE);
+    // Update the pagination UI using the "updatePaginationDiv" function
+    updatePaginationDiv(currentPage, numPages);
+  });
+
+  // Retrieve and store Pokemon in array using axios
+  $('#pokeCards').empty()
+  let response = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=810');
+  pokemons = response.data.results;
 
   paginate(currentPage, PAGE_SIZE, pokemons)
   let numPages = Math.ceil(pokemons.length / PAGE_SIZE)
   updatePaginationDiv(currentPage, numPages)
-
 
   // pop up modal when clicking on a pokemon card
   // add event listener to each pokemon card
@@ -112,6 +166,9 @@ const setup = async () => {
     currentPage = Number(e.target.value)
     paginate(currentPage, PAGE_SIZE, pokemons)
 
-
-};
-$(document).ready(setup);
+    //update pagination buttons
+    numPages = Math.ceil(pokemons.length / PAGE_SIZE);
+    updatePaginationDiv(currentPage, numPages)
+  })
+}
+$(document).ready(setup)
